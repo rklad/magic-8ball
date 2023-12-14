3fd144a76cb63b3c9351a0a8c058539c2c6909d9
rklad <525rkladerman@frhsd.com>
2023-12-14 14:55:46 -0500

    added some js

---
+++ script.js
@@ -0,0 +1,15 @@
+function shakeMagic8Ball() {
+    const question = prompt('Ask the Magic 8-Ball a question:');
+
+    if (question === null) {
+        document.getElementById('response-text').innerText = 'Please ask a question!'
+        document.getElementById('response-text').classList = 'text-danger' //changes HTML classes
+        document.getElementById('response-image').src = 'https://picsum.photos/200'
+    }
+
+    if (!question.trim()) { //.trim cuts off any spaces after the last physical character and leaves you with an empty string which is a falsy
+        alert('Please enter a valid question!')
+        return //just like break on a switch
+    } 
+
+}
