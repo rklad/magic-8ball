99ceebea740f664e054e8dcbcfc06db48aaa4267
rklad <525rkladerman@frhsd.com>
2023-12-14 14:58:00 -0500

    changed js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function shakeMagic8Ball() {
     if (question === null) {
         document.getElementById('response-text').innerText = 'Please ask a question!'
         document.getElementById('response-text').classList = 'text-danger' //changes HTML classes
-        document.getElementById('response-image').src = 'https://picsum.photos/200'
+        document.getElementById('response-image').src = '#BAD/BROKENIMG'
     }
 
     if (!question.trim()) { //.trim cuts off any spaces after the last physical character and leaves you with an empty string which is a falsy
